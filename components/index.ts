48db2b6a67ae74b4a5e8dcf68375ab472c6e6bcc
neelamchawla <neelamchawla55@gmail.com>
2023-07-09 17:50:14 +0530

    first commit

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import NavBar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./Searchbar";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import CustomFilter from "./CustomFilter";
+import ShowMore from "./ShowMore";
+
+export {
+  Hero,
+  CustomButton,
+  NavBar,
+  Footer,
+  SearchBar,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+  CustomFilter,
+  ShowMore,
+};
